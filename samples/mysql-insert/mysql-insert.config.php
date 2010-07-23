58b3fd5e7b7937b45ee3cea712dbf3a5ecf5d403
Garrett Bartley <wgbartley@gmail.com>
2010-07-23 18:10:40 -0400

    fixed "locahost" typo

--- samples/mysql-insert/mysql-insert.config.php
+++ samples/mysql-insert/mysql-insert.config.php
@@ -1,7 +1,7 @@
 <?
 /* Your MySQL database connection information */
-define('TED_DB_HOST','locahost');	# Hostname or IP address of your MySQL database
+define('TED_DB_HOST','localhost');	# Hostname or IP address of your MySQL database
 define('TED_DB_NAME','ted');		# Database name of your MySQL database
 define('TED_DB_USER','ted');		# Username to access your MySQL database
-define('TED_DB_PASS','ted5000');	# Password to access your MySQL database
+define('TED_DB_PASS','ted500');		# Password to access your MySQL database
 ?>
